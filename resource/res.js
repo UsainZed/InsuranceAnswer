9c5750de314401b6ae56eb5ed4f0d3c02066b8cf
UsainZed <1678917813@qq.com>
2020-02-11 02:16:02 +0800

    update

--- resource/res.js
+++ resource/res.js
@@ -1,2 +1,281 @@
-let questions = [{"answer": "B", "stem": "以下说法不正确的是____。", "options": [{"content": "领陆指国家主权管辖下的陆地及其底土", "option": "A"}, {"content": "领空指国家主权管辖下的全部空域及其底土", "option": "B"}], "img": null, "no": 1}, {"answer": "A", "stem": "在我国与邻国接壤的陆上边界线中，最长的是____。", "options": [{"content": "中蒙边界", "option": "A"}, {"content": "中俄边界", "option": "B"}], "img": null, "no": 2}, {"answer": "B", "stem": "“事大未必有实，则举图而委，效玺而请兵矣。献图则地削，效玺则名卑;地削则国削， 名卑则政乱矣。”这句说的是将本国的地图献给另外一个国家等于是让别人来攻打与瓜分， 放弃本国的地图等于放弃自己的国家。请问这句话是哪位中国古人说的?", "options": [{"content": "孔子", "option": "A"}, {"content": "韩非子", "option": "B"}], "img": "https://gss0.bdstatic.com/-4o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=fb529c17221f95cab2f89ae4a87e145b/b999a9014c086e0671cda62205087bf40ad1cb76.jpg", "no": 3}, {"answer": "A", "stem": "我国的领陆是否包括海南岛?", "options": [{"content": "包括", "option": "A"}, {"content": "不包括", "option": "B"}], "img": "../../resource/avatar.png", "no": 4}, {"answer": "B", "stem": "柴达木盆地是我国的四大盆地之一，其位于我国的哪个高原?", "options": [{"content": "云贵高原", "option": "A"}, {"content": "青藏高原", "option": "B"}], "img": null, "no": 5}, {"answer": "A", "stem": "我国于 1958 年 9 月 4 日发表关于领海的声明，宣布中国的领海宽度为____。", "options": [{"content": "12 海里", "option": "A"}, {"content": "200 海里", "option": "B"}], "img": null, "no": 6}, {"answer": "A", "stem": "在我国的行政区划中，哪种行政建置稳定地延续了两千多年?", "options": [{"content": "县", "option": "A"}, {"content": "省", "option": "B"}], "img": null, "no": 7}, {"answer": "B", "stem": "苏木是内蒙古自治区的一种行政区划单位，其相当于____。", "options": [{"content": "县", "option": "A"}, {"content": "乡", "option": "B"}], "img": null, "no": 8}, {"answer": "A", "stem": "我国的邻国中，哪两个国家既是我国的陆上邻国，也是海上邻国?", "options": [{"content": "越南和朝鲜", "option": "A"}, {"content": "老挝和韩国", "option": "B"}], "img": null, "no": 9}, {"answer": "B", "stem": "洞朗地区位于西藏自治区日喀则市的____。", "options": [{"content": "聂拉木县", "option": "A"}, {"content": "亚东县", "option": "B"}], "img": null, "no": 11}, {"answer": "A", "stem": "元朝时，中央政府设置什么机构管辖澎湖和台湾?", "options": [{"content": "巡检司", "option": "A"}, {"content": "市舶司", "option": "B"}], "img": null, "no": 12}, {"answer": "B", "stem": "汉代的文献一般称我国的南海为____。", "options": [{"content": "万里石塘", "option": "A"}, {"content": "涨海", "option": "B"}], "img": null, "no": 13}, {"answer": "A", "stem": "《地图管理条例》规定，在地图上绘制中华人民共和国国界，应____。", "options": [{"content": "按照中国国界线画法标准样图绘制", "option": "A"}, {"content": "按照世界各国国界线画法参考样图绘制", "option": "B"}], "img": null, "no": 14}, {"answer": "A", "stem": "中国全图是否必须表示南海诸岛?", "options": [{"content": "必须", "option": "A"}, {"content": "不必", "option": "B"}], "img": null, "no": 15}, {"answer": "B", "stem": "根据《公开地图内容表示补充规定(试行)》，绘制中国地图(含示意性中国地图)， 中国地图的图形范围上是否可以进行压盖?", "options": [{"content": "可以", "option": "A"}, {"content": "不可以", "option": "B"}], "img": null, "no": 16}, {"answer": "A", "stem": "2008 年黑瞎子岛问题的解决，标志着____的边界线全部确定。", "options": [{"content": "中俄", "option": "A"}, {"content": "中越", "option": "B"}], "img": null, "no": 17}, {"answer": "B", "stem": "在地图上，西沙群岛可否被标注为“帕拉塞尔群岛”?", "options": [{"content": "可以", "option": "A"}, {"content": "不可以", "option": "B"}], "img": null, "no": 18}, {"answer": "B", "stem": "“包公故里”是指下列哪座城市?", "options": [{"content": "开封市", "option": "A"}, {"content": "合肥市", "option": "B"}], "img": null, "no": 19}, {"answer": "A", "stem": "台湾省地图是否必须绘出钓鱼岛和赤尾屿?", "options": [{"content": "必须", "option": "A"}, {"content": "不必", "option": "B"}], "img": null, "no": 20}, {"answer": "B", "stem": "根据《地图管理条例》，出版地图的，由____送审。", "options": [{"content": "作者", "option": "A"}, {"content": "出版单位", "option": "B"}], "img": null, "no": 21}, {"answer": "A", "stem": "2015 年国务院通过《中华人民共和国澳门特别行政区行政区域图(草案)》，该草案划 定了澳门的____。", "options": [{"content": "管理水域", "option": "A"}, {"content": "管理空域", "option": "B"}], "img": null, "no": 22}, {"answer": "B", "stem": "有关我国江河湖泊之最的表述，下列关系正确的一组是____。", "options": [{"content": "最大的淡水湖——太湖", "option": "A"}, {"content": "最大的咸水湖——青海湖", "option": "B"}], "img": null, "no": 23}, {"answer": "B", "stem": "目前所通行的第五套人民币 100 元纸币的背面图案是____。", "options": [{"content": "泰山", "option": "A"}, {"content": "人民大会堂", "option": "B"}], "img": null, "no": 24}, {"answer": "A", "stem": "下列两个地名，读音正确的是____。", "options": [{"content": "乐(lào)亭县", "option": "A"}, {"content": "荥(yíng)阳县", "option": "B"}], "img": null, "no": 25}, {"answer": "B", "stem": "下列哪座城市有“成语之乡”的称誉?", "options": [{"content": "淄博市", "option": "A"}, {"content": "邯郸市", "option": "B"}], "img": null, "no": 26}, {"answer": "B", "stem": "我国跨纬度最广的省级行政区是____。", "options": [{"content": "新疆维吾尔自治区", "option": "A"}, {"content": "海南省", "option": "B"}], "img": null, "no": 27}, {"answer": "A", "stem": "有“黄河源头姊妹湖”之称的是哪两个湖泊?", "options": [{"content": "扎陵湖和鄂陵湖", "option": "A"}, {"content": "可可西里湖和饮马湖", "option": "B"}], "img": null, "no": 28}, {"answer": "B", "stem": "清康熙六年(1667 年)将江南省分置成哪两个省?", "options": [{"content": "江苏省和浙江省", "option": "A"}, {"content": "江苏省和安徽省", "option": "B"}], "img": null, "no": 29}, {"answer": "B", "stem": "唐古拉山是哪两个省级行政区的重要地理分界线?", "options": [{"content": "新疆维吾尔自治区和西藏自治区", "option": "A"}, {"content": "青海省和西藏自治区", "option": "B"}], "img": null, "no": 30}, {"answer": "B", "stem": "红军长征途中刘伯承将军与彝族首领果基小叶丹歃血结盟的故事发生在四川省的哪个 县?", "options": [{"content": "泸定县", "option": "A"}, {"content": "冕宁县", "option": "B"}], "img": null, "no": 31}, {"answer": "B", "stem": "我国的梅雨现象一般发生在下列的哪个地区?", "options": [{"content": "华北平原", "option": "A"}, {"content": "长江中下游地区", "option": "B"}], "img": null, "no": 32}, {"answer": "A", "stem": "下列哪座山脉是我国第一、二级地形阶梯的分界线?", "options": [{"content": "祁连山脉", "option": "A"}, {"content": "太行山脉", "option": "B"}], "img": null, "no": 33}, {"answer": "A", "stem": "塑造华北平原的主力是下列的哪条河流?", "options": [{"content": "黄河", "option": "A"}, {"content": "长江", "option": "B"}], "img": null, "no": 34}, {"answer": "B", "stem": "我国四大盆地中，海拔最高的是____。", "options": [{"content": "塔里木盆地", "option": "A"}, {"content": "柴达木盆地", "option": "B"}], "img": null, "no": 35}, {"answer": "A", "stem": "《长江之歌》的开头为“你从雪山走来”，歌词中的“雪山”是指____。", "options": [{"content": "唐古拉山", "option": "A"}, {"content": "念青唐古拉山", "option": "B"}], "img": null, "no": 36}, {"answer": "B", "stem": "唐代诗人胡曾有诗曰:“千里长河一旦开，亡隋波浪九天来。”诗句中的“千里长河” 是指____。", "options": [{"content": "黄河", "option": "A"}, {"content": "大运河", "option": "B"}], "img": null, "no": 37}, {"answer": "A", "stem": "历史上北宋的都城——东京即是今天的____。", "options": [{"content": "开封", "option": "A"}, {"content": "洛阳", "option": "B"}], "img": null, "no": 38}, {"answer": "B", "stem": "“雪山连绵，冰川广布”应该描述的是下列哪个地区?", "options": [{"content": "北方地区", "option": "A"}, {"content": "青藏地区", "option": "B"}], "img": null, "no": 39}, {"answer": "B", "stem": "当长江发生特大洪水时，下列哪座城市受威胁最大?", "options": [{"content": "杭州", "option": "A"}, {"content": "武汉", "option": "B"}], "img": null, "no": 40}, {"answer": "A", "stem": "下列两个省级行政区，人口密度较小的是____。", "options": [{"content": "西藏自治区", "option": "A"}, {"content": "甘肃省", "option": "B"}], "img": null, "no": 41}, {"answer": "B", "stem": "成语“泾渭分明”与以下哪个盆地有关?", "options": [{"content": "南阳盆地", "option": "A"}, {"content": "关中盆地", "option": "B"}], "img": null, "no": 42}, {"answer": "A", "stem": "丁玲小说《太阳照在桑干河上》中的“桑干河”属于什么水系?", "options": [{"content": "海河水系", "option": "A"}, {"content": "黄河水系", "option": "B"}], "img": null, "no": 43}, {"answer": "A", "stem": "雄安新区位于河北省保定市境内，包括雄县、安新、____等 3 县及周边部分区域。", "options": [{"content": "容城", "option": "A"}, {"content": "固安", "option": "B"}], "img": null, "no": 44}, {"answer": "B", "stem": "雪顿节是哪个民族的重要节日?", "options": [{"content": "裕固族", "option": "A"}, {"content": "藏族", "option": "B"}], "img": null, "no": 45}, {"answer": "A", "stem": "在下列哪座城市可以参观淮海战役纪念馆?", "options": [{"content": "徐州市", "option": "A"}, {"content": "临沂市", "option": "B"}], "img": null, "no": 46}, {"answer": "A", "stem": "“复兴号”动车组列车最先在哪条铁路线上运营?", "options": [{"content": "京沪高速铁路", "option": "A"}, {"content": "京广高速铁路", "option": "B"}], "img": null, "no": 47}, {"answer": "B", "stem": "下列铁路干线，与长江干流不相交的是____。", "options": [{"content": "京沪高速铁路", "option": "A"}, {"content": "沪昆高速铁路", "option": "B"}], "img": null, "no": 48}, {"answer": "B", "stem": "下列哪座城市被中国气象学会授予“中国凉都”的称号?", "options": [{"content": "贵阳市", "option": "A"}, {"content": "六盘水市", "option": "B"}], "img": null, "no": 49}, {"answer": "A", "stem": "浙江省的____创办了国内的首个地理信息小镇。", "options": [{"content": "德清县", "option": "A"}, {"content": "桐乡市", "option": "B"}], "img": null, "no": 50}, {"answer": "B", "stem": "中国人民解放军建军 90 周年阅兵在朱日和训练基地举行，该基地位于____。", "options": [{"content": "新疆维吾尔自治区", "option": "A"}, {"content": "内蒙古自治区", "option": "B"}], "img": null, "no": 51}, {"answer": "A", "stem": "地理歌谣“东方旭日初生，西陲星斗满天”说的是我国疆域____。", "options": [{"content": "东西跨度大", "option": "A"}, {"content": "南北跨度大", "option": "B"}], "img": null, "no": 52}, {"answer": "B", "stem": "“大杂小聚居”描述的是我国的____分布特征。", "options": [{"content": "人口", "option": "A"}, {"content": "民族", "option": "B"}], "img": null, "no": 53}, {"answer": "B", "stem": "塞罕坝精神植根于塞罕坝的万亩的土地，塞罕坝位于河北省的哪个县境内?", "options": [{"content": "丰宁满族自治县", "option": "A"}, {"content": "围场满族蒙古族自治县", "option": "B"}], "img": null, "no": 54}, {"answer": "A", "stem": "习近平总书记是在哪里提出了“绿水青山就是金山银山”的科学论断?", "options": [{"content": "浙江省安吉县", "option": "A"}, {"content": "福建省宁德市", "option": "B"}], "img": null, "no": 55}, {"answer": "A", "stem": "我国的主要航天中心有四川西昌、甘肃酒泉、山西太原和____。", "options": [{"content": "海南文昌", "option": "A"}, {"content": "海南海口", "option": "B"}], "img": null, "no": 56}, {"answer": "A", "stem": "以下水域属于我国内水的是____。", "options": [{"content": "渤海", "option": "A"}, {"content": "黄海", "option": "B"}], "img": null, "no": 57}, {"answer": "B", "stem": "曾经的中共中央所在地西柏坡位于河北省的哪个县?", "options": [{"content": "正定县", "option": "A"}, {"content": "平山县", "option": "B"}], "img": null, "no": 58}, {"answer": "B", "stem": "我国的大地原点位于陕西省的____境内。", "options": [{"content": "西安市", "option": "A"}, {"content": "咸阳市", "option": "B"}], "img": null, "no": 59}, {"answer": "A", "stem": "我国的四大高原中，哪座高原以喀斯特地貌著称?", "options": [{"content": "云贵高原", "option": "A"}, {"content": "青藏高原", "option": "B"}], "img": null, "no": 60}]
+let questions = [{
+  "answer": "B",
+  "stem": "前面这盘野味看起来好美味啊，口水都在流了，你会____。",
+  "options": [{
+    "content": "嗯，真香",
+    "option": "A"
+  },
+  {
+    "content": "不，我不饿",
+    "option": "B"
+  }],
+  "img": "../../resource/question1.png",
+  "no": 1
+},
+{
+  "answer": "A",
+  "stem": "早上测量体温超 37.5 度，但自我感觉良好，糟糕了，上班快迟到了？",
+  "options": [{
+    "content": "我爱工作，工作使我快乐",
+    "option": "A"
+  },
+  {
+    "content": "和老板请假叭 ",
+    "option": "B"
+  }],
+  "img": "../../resource/question2.png",
+  "no": 2
+},
+{
+  "answer": "B",
+  "stem": "“有一个紧急多人现场会议需要召开，怎么办？",
+  "options": [{
+    "content": "生命成可贵，不开才安心",
+    "option": "A"
+  },
+  {
+    "content": "都2020了，线上会议它不香吗",
+    "option": "B"
+  }],
+  "img": "../../resource/question3.png",
+  "no": 3
+},
+{
+  "answer": "A",
+  "stem": "我旁边的同事出现了咳嗽症状，该怎么办？",
+  "options": [{
+    "content": "让他咳嗽、打喷嚏用胳膊肘遮住口鼻",
+    "option": "A"
+  },
+  {
+    "content": "我戴好防护口罩，离他至少一米远",
+    "option": "B"
+  }],
+  "img": "../../resource/question4.png",
+  "no": 4
+},
+{
+  "answer": "B",
+  "stem": "疫情期间，若想要办理华夏保险承保、保全等业务",
+  "options": [{
+    "content": "去柜台找专业的小姐姐办理 ",
+    "option": "A"
+  },
+  {
+    "content": "动动手指官微自助办理",
+    "option": "B"
+  }],
+  "img": "../../resource/question5.png",
+  "no": 5
+},
+{
+  "answer": "A",
+  "stem": "复工返程好累呀，到家了先来个葛优躺",
+  "options": [{
+    "content": "养好精神才能与病毒抗争",
+    "option": "A"
+  },
+  {
+    "content": "还是洗手消毒，隔离衣物才保险",
+    "option": "B"
+  }],
+  "img": "../../resource/question6.png",
+  "no": 6
+},
+{
+  "answer": "B",
+  "stem": "快看前面有辆湖北车",
+  "options": [{
+    "content": "雨女无瓜，走吧",
+    "option": "A"
+  },
+  {
+    "content": "报告社区或物业",
+    "option": "B"
+  }],
+  "img": "../../resource/question7png",
+  "no": 7
+},
+{
+  "answer": "B",
+  "stem": "害，药店酒精都卖光了，我可怎么办。咦，老舅年前带了一瓶96伏特加",
+  "options": [{
+    "content": "好嗨哟，直接拿来消毒吧",
+    "option": "A"
+  },
+  {
+    "content": "听说75%酒精最管用，还是兑点水吧",
+    "option": "B"
+  }],
+  "img": "../../resource/question8.png",
+  "no": 8
+},
+{
+  "answer": "A",
+  "stem": "钟老说了，病毒可能通过粪口传播，来给马桶也消消毒吧",
+  "options": [{
+    "content": "洁厕灵和84消毒水混合使用，一定威力无比",
+    "option": "A"
+  },
+  {
+    "content": "先用洁厕灵，再上84消毒水",
+    "option": "B"
+  }],
+  "img": "../../resource/question9.png",
+  "no": 9
+},
+{
+  "answer": "B",
+  "stem": "盼星星，盼月亮，新买的大衣终于寄到手了",
+  "options": [{
+    "content": "立马拆开包裹先试试，气质一定max ",
+    "option": "A"
+  },
+  {
+    "content": "先把包裹放阳台晾凉，洗手消毒最重要 ",
+    "option": "B"
+  }],
+  "img": "../../resource/question10.png",
+  "no": 10
+},
+{
+  "answer": "A",
+  "stem": "天气真好，我要出去透透气",
+  "options": [{
+    "content": "穿上皮大衣，我就是这条gai最靓的仔  ",
+    "option": "A"
+  },
+  {
+    "content": "还是喜欢毛衣，低调有内涵 ",
+    "option": "B"
+  }],
+  "img": "../../resource/question11.png",
+  "no": 11
+},
+{
+  "answer": "B",
+  "stem": "爸妈在家熏醋，这味道真酸爽",
+  "options": [{
+    "content": "听说熏醋能杀毒，忍一忍吧",
+    "option": "A"
+  },
+  {
+    "content": "太刺鼻了，消停消停吧",
+    "option": "B"
+  }],
+  "img": "../../resource/question12.png",
+  "no": 12
+},
+{
+  "answer": "A",
+  "stem": "这家药店居然还有这么多双黄",
+  "options": [{
+    "content": "OMG，买它",
+    "option": "A"
+  },
+  {
+    "content": "还是买瓶维生素保障免疫力吧 ",
+    "option": "B"
+  }],
+  "img": "../../resource/question13.png",
+  "no": 13
+},
+{
+  "answer": "A",
+  "stem": "听说华夏保险推出了针对新冠的守护保保险计划",
+  "options": [{
+    "content": "奥利给，我要来一份  ",
+    "option": "A"
+  },
+  {
+    "content": "我才不要，我不会感染",
+    "option": "B"
+  }],
+  "img": "../../resource/question14.png",
+  "no": 14
+},
+{
+  "answer": "B",
+  "stem": "李兰娟院士说56°C持续30分钟可以杀毒，那我来给衣服消消毒",
+  "options": [{
+    "content": "用挂烫机熨一熨，我真是小天才",
+    "option": "A"
+  },
+  {
+    "content": "还是用勾兑消毒液洗一洗来得快",
+    "option": "B"
+  }],
+  "img": "../../resource/question15.png",
+  "no": 15
+},
+{
+  "answer": "A",
+  "stem": "为了保持健康的生活状态",
+  "options": [{
+    "content": "坚持户外慢跑，提升心肺功能",
+    "option": "A"
+  },
+  {
+    "content": "还是宅在家，早睡早起",
+    "option": "B"
+  }],
+  "img": "../../resource/question16.png",
+  "no": 16
+},
+{
+  "answer": "B",
+  "stem": "为了增强抵抗病毒的免疫力",
+  "options": [{
+    "content": "肉蛋熟食，多吃果蔬 ",
+    "option": "A"
+  },
+  {
+    "content": "多吃抗生素",
+    "option": "B"
+  }],
+  "img": "../../resource/question17.png",
+  "no": 17
+},
+{
+  "answer": "B",
+  "stem": "“准备些口罩上班用用",
+  "options": [{
+    "content": "N95走起，全面防护",
+    "option": "A"
+  },
+  {
+    "content": "一次性医用口罩刚刚好",
+    "option": "B"
+  }],
+  "img": "../../resource/question18.png",
+  "no": 18
+},
+{
+  "answer": "A",
+  "stem": "口罩稀缺就像国宝，如需重复使用",
+  "options": [{
+    "content": "喷点酒精给它消消毒 ",
+    "option": "A"
+  },
+  {
+    "content": "挂在洁净干燥通风处，让它也透透气吧",
+    "option": "B"
+  }],
+  "img": "../../resource/question19.png",
+  "no": 19
+},
+{
+  "answer": "B",
+  "stem": "我太南了，口罩都变形了",
+  "options": [{
+    "content": "由内向外折好，丢入黄色垃圾桶",
+    "option": "A"
+  },
+  {
+    "content": "由内向外折好，丢入红色垃圾桶 ",
+    "option": "B"
+  }],
+  "img": "../../resource/question20.png",
+  "no": 20
+}] 
 module.exports = questions
