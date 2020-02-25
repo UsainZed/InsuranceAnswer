7e9cbcc4b3d1b68fe56406a94d8a998873f3aa7a
UsainZed <1678917813@qq.com>
2020-02-26 00:13:57 +0800

    修复音乐问题

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //app.js
 App({
   onLaunch: function () {
+  },
+  onShow: function () {
     const innerAudioContext = wx.createInnerAudioContext();
     innerAudioContext.autoplay = true;
     innerAudioContext.src = 'https://img.tukuppt.com/newpreview_music/09/01/72/5c8a08dc4956424741.mp3';
     innerAudioContext.loop = true;
-    innerAudioContext.play();
-  },
+    innerAudioContext.play(); },
   globalData: {
     userInfo: null
   }
